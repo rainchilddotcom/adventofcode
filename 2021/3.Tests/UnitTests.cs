f28cee9edf9b136bf55be33dc65a7f0a5fddaa7f
rainchilddotcom <95854040+rainchilddotcom@users.noreply.github.com>
2021-12-10 16:02:19 +1100

    Day 3

---
+++ 2021/3.Tests/UnitTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace _3.Tests
+{
+    public class UnitTests
+    {
+        readonly List<DiagnosticFlags> diagnostics = new List<int>() {
+                0b00100,
+                0b11110,
+                0b10110,
+                0b10111,
+                0b10101,
+                0b01111,
+                0b00111,
+                0b11100,
+                0b10000,
+                0b11001,
+                0b00010,
+                0b01010
+            }.Cast<DiagnosticFlags>().ToList();
+
+        [Fact]
+        public void TestPart1()
+        {
+            var result = _3.Program.Part1(diagnostics, DiagnosticFlags.Flag5);
+
+            Assert.Equal(198, result);
+        }
+
+        [Fact]
+        public void TestPart2()
+        {
+            var result = _3.Program.Part2(diagnostics, DiagnosticFlags.Flag5);
+
+            Assert.Equal(230, result);
+        }
+
+        [Fact]
+        public void TestPart2Oxygen()
+        {
+            var result = _3.Program.CalculateRating(diagnostics, DiagnosticFlags.Flag5, _3.Program.Oxygen);
+
+            Assert.Equal(23, (int) result);
+        }
+
+        [Fact]
+        public void TestPart2CO2()
+        {
+            var result = _3.Program.CalculateRating(diagnostics, DiagnosticFlags.Flag5, _3.Program.CO2);
+
+            Assert.Equal(10, (int)result);
+        }
+    }
+}
---
+++ 2021/3/DiagnosticFlags.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _3
+{
+    [Flags]
+    public enum DiagnosticFlags
+    {
+        Flag12 = 1 << 11,
+        Flag11 = 1 << 10,
+        Flag10 = 1 << 9,
+        Flag9 = 1 << 8,
+        Flag8 = 1 << 7,
+        Flag7 = 1 << 6,
+        Flag6 = 1 << 5,
+        Flag5 = 1 << 4,
+        Flag4 = 1 << 3,
+        Flag3 = 1 << 2,
+        Flag2 = 1 << 1,
+        Flag1 = 1 << 0
+    }
+}
