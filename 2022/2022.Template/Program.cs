b101994d36637a5f52abf593f8daec89c90d6a42
rainchilddotcom <95854040+rainchilddotcom@users.noreply.github.com>
2022-12-04 21:25:44 +1100

    Daily Project Template

---
+++ 2022/2022.Template/Program.cs
@@ -0,0 +1,42 @@
+﻿CopyTemplate(@".\template", "5");
+
+static void CopyTemplate(string templateDir, string day)
+{
+    var workingDir = new DirectoryInfo(".");
+    while (workingDir.FullName.Contains(@"\bin"))
+        workingDir = workingDir.Parent;
+
+    var dir = workingDir
+        .GetDirectories(templateDir)
+        .Single();
+
+    if (!dir.Exists)
+        throw new DirectoryNotFoundException($"Template directory not found: {dir.FullName}");
+
+    CopyDirectory(dir.FullName, workingDir.Parent.FullName, day);
+}
+
+static void CopyDirectory(string sourceDir, string destDir, string day)
+{
+    var dir = new DirectoryInfo(sourceDir);
+    string destinationDir = destDir.Replace("{X}", day);
+
+    Console.WriteLine($"Creating {destinationDir}");
+    Directory.CreateDirectory(destinationDir);
+
+    foreach (FileInfo file in dir.GetFiles())
+    {
+        string targetFilePath = Path.Combine(destinationDir, file.Name.Replace("{X}", day).Replace(@".\template", @".."));
+        Console.WriteLine($"Copying template to {targetFilePath}");
+        var content = File.ReadAllText(file.FullName);
+        content = content.Replace("{X}", day);
+        File.WriteAllText(targetFilePath, content);
+    }
+
+    DirectoryInfo[] dirs = dir.GetDirectories();
+    foreach (DirectoryInfo subDir in dirs)
+    {
+        string newDestinationDir = Path.Combine(destinationDir, subDir.Name);
+        CopyDirectory(subDir.FullName, newDestinationDir, day);
+    }
+}
---
+++ 2022/2022.Template/template/{X}.Tests/Day{X}Tests.cs
@@ -0,0 +1,14 @@
+namespace _{X}.Tests
+{
+    public class Day{X}Tests
+    {
+        const string testInput =
+@"
+";
+        
+        [Test]
+        public void Test()
+        {
+        }
+    }
+}
